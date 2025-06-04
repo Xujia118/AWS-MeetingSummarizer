5aaebe7669f200cd643b39f172dd1247a2c97588
Xujia118 <116283847+Xujia118@users.noreply.github.com>
2025-06-04 12:45:58 -0400

    Api layer (#7)
    * Added upload_audio.py lambda
    
    * Made response body in upload_audio.py a string to conform to API Gateway requirement
    
    * Aded get_summary lambda + modified table name
    
    * Fixed a mistake in apigw integration lambda
    
    * Send meeting_id to frontend at upload so that we can fetch from db with the same meeting_id
    
    * Added summary text to get_summaries lambda

--- hello_cdk/api_stack.py
+++ hello_cdk/api_stack.py
@@ -57,6 +57,7 @@ class APIStack(Stack):
 
         # Permissions
         bucket.grant_put(upload_audio_lambda)
+        bucket.grant_read(get_summary_lambda)
         table.grant_read_data(get_summary_lambda)
 
         # API Gateway
@@ -94,7 +95,7 @@ class APIStack(Stack):
                 "method.request.path.meeting_id": True
             }
         )
-        
+
         # Output the API endpoint URL
         self.api_url = api.url
 
